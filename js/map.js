4ce19e6c0884ef72d35b732012ee365016235c7e
Nicolas Larrea <nicolaslarrea@gmail.com>
2014-04-24 19:40:41 +0200

    Integrated code to draw charts when a user clicks on a country.

--- js/map.js
+++ js/map.js
@@ -5,6 +5,7 @@
     var countryIndices = {};
     var evtScale, evtTrans;
     var fillRange;
+    var mouseHoverOverCountry;
 
     //load csv file
     var loadData = function(redrawAfterLoading) {
@@ -29,6 +30,7 @@
                 }
             });
 
+
             fillRange = d3.scale.linear()
                 .domain([minScale, maxScale])
                 .range(["cyan", "steelblue"]);
@@ -39,12 +41,14 @@
     };
     loadData(false);
     //upon data selection change -reload the data.
-    $("#urbpop, #agrlnd").change(function () {
+    $("#urbpop, #agrlnd, #health").change(function () {
         if ($(this).val() === "urbpop") {
             dataFile = "data/SP.URB.TOTL.IN.ZS_Indicator_en_csv_v2.csv";
 
         }else if ($(this).val() === "agrlnd") {
             dataFile = "data/ag.lnd.agri.zs_Indicator_en_csv_v2.csv";
+        }else if ($(this).val() === "health") {
+            dataFile = "data/sh.xpd.totl.zs_Indicator_en_csv_v2.csv";
         }else{
             return;
         }
@@ -52,7 +56,7 @@
     });
     //initialize slider
     $("#slider").slider();
-//change year
+    //change year
     $("#slider").on('slide', function(slideEvt) {
         $("#sliderVal").text(slideEvt.value);
         if(slideEvt.value[0]){
@@ -110,7 +114,7 @@
     });
 
     //function draw(countries, neighbors) {
-    function draw(countries, neighbors) {
+    function draw(countries) {
 
         svg.append("path")
             .datum(graticule)
@@ -148,7 +152,7 @@
         //tooltips
         country
             .on("mousemove", function(d,i) {
-
+                mouseHoverOverCountry = d;
                 var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
 
                 var ctry = countryIndices[d.properties.name];
@@ -163,6 +167,7 @@
 
             })
             .on("mouseout",  function(d,i) {
+                mouseHoverOverCountry = undefined;
                 tooltip.classed("hidden", true);
             });
     }
@@ -214,30 +219,183 @@
 
     //geo translation on mouse click in map
     function click() {
-        var latlon = projection.invert(d3.mouse(this));
-        console.log(latlon);
+        //var latlon = projection.invert(d3.mouse(this));
+        //console.log(latlon);
+        if(mouseHoverOverCountry) {
+            //drawChart("Germany");
+            drawChart(mouseHoverOverCountry.properties.name)
+        }
+
+
     }
 
-//function to add points and text to the map (used in plotting capitals)
-//    function addpoint(lat,lon,text) {
-//        var gpoint = g.append("g").attr("class", "gpoint");
-//        var x = projection([lat,lon])[0];
-//        var y = projection([lat,lon])[1];
-//        gpoint.append("svg:circle")
-//            .attr("cx", x)
-//            .attr("cy", y)
-//            .attr("class","point")
-//            .attr("r", 1.5);
-//        //conditional in case a point has no associated text
-//        if(text.length>0){
-//
-//            gpoint.append("text")
-//                .attr("x", x+2)
-//                .attr("y", y+2)
-//                .attr("class","text")
-//                .text(text);
-//        }
-//
-//    }
+
+
+//__start chart code
+function drawChart(countryName){
+    $("#myModalLabel").text(countryName);
+    $("#countryChart" ).empty();
+    var margin = {top: 80, right: 80, bottom: 80, left: 80},
+        width = 550 - margin.left - margin.right,
+        height = 360 - margin.top - margin.bottom;
+
+    var bisectDate = d3.bisector(function(d) { return d.year; }).left,
+        formatValue = d3.format(".2f");
+
+    // Scales and axes. Note the inverted domain for the y-scale: bigger is up!
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .tickSize(-height)
+        .tickFormat(function(d) {return d;}) // Needed to remove the decimal separator (eg. 2001 would become 2,001 otherwise)
+        .tickSubdivide(false);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .ticks(4)
+        .orient("right");
+
+    // An area generator, for the light fill.
+    var area = d3.svg.area()
+        .interpolate("monotone")
+        .x(function(d) { return x(d.year); })
+        .y0(height)
+        .y1(function(d) { return y(d.value); });
+
+    // A line generator, for the dark stroke.
+    var line = d3.svg.line()
+        .interpolate("monotone")
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y(d.value); });
+
+    /**
+     * Main function to draw the graph
+     */
+    function draw(country) {
+        var values = loadCountry(country);
+
+        // Compute the minimum and maximum date, and the maximum price.
+        x.domain([1960,2011]);
+        y.domain([0, d3.max(values, function(d) { return d.value; })]).nice();
+
+        // Add an SVG element with the desired dimensions and margin.
+        var svg = d3.select("#countryChart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        // Add the clip path.
+        svg.append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", width)
+            .attr("height", height);
+
+        // Add the area path.
+        svg.append("path")
+            .attr("class", "area")
+            .attr("clip-path", "url(#clip)")
+            .attr("d", area(values));
+
+        // Add the x-axis.
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add the y-axis.
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + width + ",0)")
+            .call(yAxis);
+
+        // Add the line path.
+        svg.append("path")
+            .attr("class", "line")
+            .attr("clip-path", "url(#clip)")
+            .attr("d", line(values));
+
+        // Add a small label for the symbol name.
+        svg.append("text")
+            .attr("x", width - 6)
+            .attr("y", height - 6)
+            .style("text-anchor", "end")
+            .text(country);
+
+        //// Hover over
+        var focus = svg.append("g")
+            .attr("class", "focus")
+            .style("display", "none");
+
+        focus.append("circle")
+            .attr("r", 4.5);
+
+        focus.append("text")
+            .attr("x", 9)
+            .attr("dy", ".35em");
+
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .on("mouseover", function() { focus.style("display", null); })
+            .on("mouseout", function() { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
+
+        /**
+         * Function that repositions the hover over circle.
+         */
+        function mousemove() {
+            var x0 = x.invert(d3.mouse(this)[0]),
+                i = bisectDate(values, x0, 1), // Find the year in the values array
+                d0 = values[i - 1],
+                d1 = values[i],
+                d = x0 - d0.year > d1.year - x0 ? d1 : d0;
+            focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")"); // Position the circle
+            focus.select("text").text(d.year + ': ' + formatValue(d.value)); // Add some text next to the circle
+        }
+
+    }
+
+    /**
+     * Loads the data for a specific country in an array of the correct format.
+     */
+    function loadCountry(country) {
+        var ret = [];
+
+        for (var key in countryIndices[country]) {
+            if(!isNaN(parseInt(key))) {
+                var val = parseFloat(countryIndices[country][key]);
+                if(isNaN(val)) {
+                    val = 0;
+                }
+                ret.push({'year':parseInt(key),'value':val});
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Format the value as a positive number
+     */
+    function type(d) {
+        d.value = +d.value;
+        return d;
+    }
+    draw(countryName);
+
+    $('#myModal').modal('show');
+
+
+
+}
+//__end chart code
 
 }());
